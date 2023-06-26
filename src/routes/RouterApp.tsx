f611fbc76c430d97173c2ec252a2ac49b3cb7bc0
jeancs21 <40588173+jeancs21@users.noreply.github.com>
2023-06-26 18:03:06 -0400

    remove import from router

--- src/routes/RouterApp.tsx
+++ src/routes/RouterApp.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Dashboard from '../pages/Dashboard/Dashboard'
 import CompanyInventory from '../pages/Inventory/CompanyInventory'
