33a7e1a10d620b4ebd4c378f420251083050cc98
jeancs21 <40588173+jeancs21@users.noreply.github.com>
2023-06-26 16:12:00 -0400

    Allow product pdf downloads

--- src/pages/Inventory/components/ProductFetcher.tsx
+++ src/pages/Inventory/components/ProductFetcher.tsx
@@ -4,6 +4,8 @@ import { AppStore } from '../../../redux/store'
 import CardList from './CardList'
 import EmptyInventoryMessage from './EmptyInventoryMessage'
 import { Company } from '../../models/company'
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 type Props = {
     company: Company
@@ -14,13 +16,34 @@ const ProductFetcher:FunctionComponent<Props> = (props) => {
     const productList = useSelector((store: AppStore) => store.product)
     const filteredProducts = productList.filter(product => product.company === props.company.nit)
 
+    const downloadInventoryAsPDF = async () => {
+        const inventoryContainer = document.getElementById('inventory-container');
+        if (!inventoryContainer) {
+          return;
+        }
+    
+        const canvas = await html2canvas(inventoryContainer);
+    
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF('p', 'mm', 'a4');
+        const width = pdf.internal.pageSize.getWidth();
+        const height = pdf.internal.pageSize.getHeight();
+        pdf.addImage(imgData, 'PNG', 0, 0, width, height);
+        pdf.save('inventory.pdf');
+      };
+
   return (
     <>
         <div className='flex flex-col justify-center'>
             <h1 className='text-3xl self-center p-8'>Lista de productos</h1>
             <div className='self-center container p-8 rounded-md'>
                 {filteredProducts.length > 0 ?
-                    <CardList products={filteredProducts}/>
+                    <>
+                        <div id='inventory-container'>
+                            <CardList products={filteredProducts}/>
+                        </div>
+                        <button onClick={downloadInventoryAsPDF}>Descargar inventario en PDF</button>
+                    </>
                     :
                     <EmptyInventoryMessage />
                 }
